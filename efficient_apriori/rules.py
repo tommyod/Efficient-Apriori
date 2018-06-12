be0e48418da4ad1ff8e8966912aa2ae17b0dba99
tommyod <tommy.odland@gmail.com>
2018-06-12 17:31:54 +0200

    Made support a float between 0 and 1

--- efficient_apriori/rules.py
+++ efficient_apriori/rules.py
@@ -4,15 +4,39 @@
 Created on Mon Jun 11 20:53:10 2018
 
 @author: tommy
+
+
+
+https://www.kaggle.com/c/instacart-market-basket-analysis/data
 """
+
+from efficient_apriori.itemsets import itemsets_from_transactions
+
+def generate_rules_naively():
+    """
+    Generate rules naively, for testing.
+    """
+    pass
 
 def genrules():
     """
     The naive algorithm from the original paper.
     """
-    
+    pass
     
 def ap_genrules():
     """
     The faster algorithm from the original paper.
     """
+    pass
+
+
+if __name__ == '__main__':
+    
+    transactions = ['cbd',
+                    'abc',
+                    'abd',
+                    'cba',
+                    'abf',
+                    'acb',
+                    'cbf']
