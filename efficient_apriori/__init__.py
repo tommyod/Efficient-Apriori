d99c6c54e79963b2836463aca65ebc4597d66180
tommyod <tommy.odland@gmail.com>
2021-10-17 11:14:36 +0200

    incremented version to 2.0.0

--- efficient_apriori/__init__.py
+++ efficient_apriori/__init__.py
@@ -6,7 +6,7 @@ Implementation of the Apriori algorithm.
 
 # We use semantic versioning
 # See https://semver.org/
-__version__ = "1.1.1"
+__version__ = "2.0.0"
 
 import sys
 from efficient_apriori.apriori import apriori
