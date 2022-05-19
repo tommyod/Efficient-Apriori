01d174379c51758aa2f6d2926b473124928dc631
Tommy <10076072+tommyod@users.noreply.github.com>
2022-05-19 08:12:05 +0200

    Changed lisence (#52)

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         # 'Intended Audience :: End Users/Desktop',
         # 'Intended Audience :: Healthcare Industry',
         # Pick your license as you wish (should match "license" above)
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 3.6",
