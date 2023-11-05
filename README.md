9ec4448dcb37c7410ff81c0062a805ba6750b7fc
Tommy <10076072+tommyod@users.noreply.github.com>
2023-11-05 08:21:57 +0100

    Python 3.12 (#63)
    Co-authored-by: tommyod <tommy.odland>

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Efficient-Apriori ![Build Status](https://github.com/tommyod/Efficient-Apriori/workflows/Python%20CI/badge.svg?branch=master) [![PyPI version](https://badge.fury.io/py/efficient-apriori.svg)](https://pypi.org/project/efficient-apriori/) [![Documentation Status](https://readthedocs.org/projects/efficient-apriori/badge/?version=latest)](https://efficient-apriori.readthedocs.io/en/latest/?badge=latest) [![Downloads](https://pepy.tech/badge/efficient-apriori)](https://pepy.tech/project/efficient-apriori) [![Black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/ambv/black)
 
-An efficient pure Python implementation of the Apriori algorithm. Works with Python 3.7+.
+An efficient pure Python implementation of the Apriori algorithm.
 
 The apriori algorithm uncovers hidden structures in categorical data.
 The classical example is a database containing purchases from a supermarket.
--- setup.py
+++ setup.py
@@ -30,8 +30,7 @@ setup(
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
     version=VERSION,
-    description=" An efficient Python implementation of the Apriori \
-    algorithm.",
+    description=" An efficient Python implementation of the Apriori algorithm.",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     # The project's main homepage.
@@ -55,11 +54,11 @@ setup(
         "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
