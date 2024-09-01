9c1d24363e3f86ac2dd14a2871997d78620cc084
tommyod <tommy.odland>
2024-09-01 20:30:00 +0200

    version

--- efficient_apriori/__init__.py
+++ efficient_apriori/__init__.py
@@ -4,14 +4,15 @@
 Implementation of the Apriori algorithm.
 """
 
-# We use semantic versioning
-# See https://semver.org/
-__version__ = "2.0.4"
-
+import importlib.metadata
 import sys
 from efficient_apriori.apriori import apriori
 from efficient_apriori.itemsets import itemsets_from_transactions
 from efficient_apriori.rules import Rule, generate_rules_apriori
+
+# We use semantic versioning
+# See https://semver.org/
+__version__ = importlib.metadata.version("efficient_apriori")
 
 __all__ = ["apriori", "itemsets_from_transactions", "Rule", "generate_rules_apriori"]
 
@@ -25,8 +26,3 @@ def run_tests():
 
     base, _ = os.path.split(__file__)
     pytest.main(args=[base, "--doctest-modules"])
-
-
-if (sys.version_info[0] < 3) or (sys.version_info[1] < 7):
-    msg = "The `efficient_apriori` package only works for Python 3.7+."
-    raise Exception(msg)
